48f4b2dc34037b0d1f8207caccbaba4998d5afd1
Kundan <kundank191@gmail.com>
2018-10-05 23:48:23 +0530

    feat : add place autocomplete activity

--- app/src/main/java/com/example/kunda/aqiapp/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/example/kunda/aqiapp/ui/fragments/HomeFragment.java
@@ -47,9 +47,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment, Initialize views
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-
         pollutantsDataRV = rootView.findViewById(R.id.rv_pollutants);
         pollutantsDataRV.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.HORIZONTAL,false));
         //Initialize viewModelFactory and viewModel
--- app/src/main/java/com/example/kunda/aqiapp/ui/fragments/SavedLocationsFragment.java
+++ app/src/main/java/com/example/kunda/aqiapp/ui/fragments/SavedLocationsFragment.java
@@ -1,67 +1,86 @@
 package com.example.kunda.aqiapp.ui.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.kunda.aqiapp.R;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link SavedLocationsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class SavedLocationsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    private String TAG = "Hello";
 
     public SavedLocationsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SavedLocationsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SavedLocationsFragment newInstance(String param1, String param2) {
-        SavedLocationsFragment fragment = new SavedLocationsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_locations, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_saved_locations, container, false);
+        FloatingActionButton button = rootView.findViewById(R.id.fab_add_place);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addPlace();
+            }
+        });
+        return rootView;
     }
 
+    private void addPlace(){
+
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                            .build(Objects.requireNonNull(getActivity()));
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            Toast.makeText(getContext(),"Error getting place info",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(Objects.requireNonNull(getContext()), data);
+                Log.i(TAG, "Place: " + place.getName() + " " + place.getLatLng());
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(Objects.requireNonNull(getContext()), data);
+                Toast.makeText(getContext(),"Error getting place info",Toast.LENGTH_SHORT).show();
+                Log.i(TAG, status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+                Log.d("Handle error","Error Handled");
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
