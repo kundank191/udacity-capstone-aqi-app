388da80ebf548b0b807fb58202a1e046adeaff31
Kundan <kundank191@gmail.com>
2018-11-20 23:12:32 +0530

    feat : widget shows home location data

--- app/src/main/java/com/example/kunda/aqiapp/ui/widget/HomeDataAppWidget.java
+++ app/src/main/java/com/example/kunda/aqiapp/ui/widget/HomeDataAppWidget.java
@@ -6,21 +6,17 @@ import android.content.Context;
 import android.widget.RemoteViews;
 
 import com.example.kunda.aqiapp.R;
-import com.example.kunda.aqiapp.data.AirQualityRepository;
 import com.example.kunda.aqiapp.data.database.LocationData;
 import com.example.kunda.aqiapp.utils.InjectorUtils;
 
 /**
  * Implementation of App Widget functionality.
- */
+        */
 public class HomeDataAppWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+                                int appWidgetId, LocationData homeLocationData) {
 
-        // Get the data to be shown in the widget.
-        AirQualityRepository repository = InjectorUtils.provideAirQualityRepository(context);
-        LocationData homeLocationData = repository.getHomeLocationData();
         String text = "some Default Text";
         if (homeLocationData != null) {
             text = homeLocationData.getLocationName();
@@ -34,11 +30,17 @@ public class HomeDataAppWidget extends AppWidgetProvider {
     }
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+        InjectorUtils.provideAppExecutors().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                LocationData homeLocationData = InjectorUtils.provideAirQualityRepository(context).getHomeLocationData();
+                for (int appWidgetId : appWidgetIds) {
+                    updateAppWidget(context, appWidgetManager, appWidgetId,homeLocationData);
+                }
+            }
+        });
     }
 
     @Override
